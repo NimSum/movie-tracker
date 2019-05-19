eabd16a6789430ded5fade03f4bc2258aaad8af7
Nimrod Garcia <nimm.garcia@gmail.com>
2019-05-19 11:48:09 -0600

    Updates

---
+++ precache-manifest.446131168a5aae9ee33b416388691d59.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e079142a8e6d556ed7fef40e4321da7b",
+    "url": "/movie-tracker/index.html"
+  },
+  {
+    "revision": "0a3088db0f6079b58270",
+    "url": "/movie-tracker/static/css/main.4e4371d4.chunk.css"
+  },
+  {
+    "revision": "478517fd843c56cdfda1",
+    "url": "/movie-tracker/static/js/2.e55cf14f.chunk.js"
+  },
+  {
+    "revision": "0a3088db0f6079b58270",
+    "url": "/movie-tracker/static/js/main.fda0b27a.chunk.js"
+  },
+  {
+    "revision": "def1329353ad87605c3a",
+    "url": "/movie-tracker/static/js/runtime~main.e9d78e93.js"
+  },
+  {
+    "revision": "4bc306eb22928542511dde0eef726cc5",
+    "url": "/movie-tracker/static/media/active.4bc306eb.svg"
+  },
+  {
+    "revision": "a8ba11ab610d0b7852dff5a7d6ba33e7",
+    "url": "/movie-tracker/static/media/film.a8ba11ab.png"
+  },
+  {
+    "revision": "9e0d17aa3b88992215a0882fce7f915b",
+    "url": "/movie-tracker/static/media/hamburger.9e0d17aa.svg"
+  },
+  {
+    "revision": "5cff0a49370a403bddefe8e2c10234c6",
+    "url": "/movie-tracker/static/media/inactive.5cff0a49.svg"
+  }
+]);
